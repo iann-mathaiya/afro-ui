d5f2080b52319bb05546c405cbd9556050972c93
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-01 15:32:44 +0300

    basic structure added

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import EvilDesignCaseStudies from '@/components/EvilDesignCaseStudies'
 
 export default function page() {
   return (
-    <div>Cards</div>
+    <main className='mx-auto max-w-7xl min-h-screen p-4 lg:p-12'>
+      <EvilDesignCaseStudies />
+    </main>
   )
 }
--- components/EvilDesignCaseStudies.tsx
+++ components/EvilDesignCaseStudies.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
 
 export default function EvilDesignCaseStudies() {
   return (
-    <div>EvilDesignCaseStudies</div>
+    <section className="border-t border-t-slate-300 pt-2">
+      <span className="scroll-m-20 text-sm uppercase text-slate-500">Success Stories</span>
+
+      <div className="my-4 lg:my-6 flex items-center justify-between">
+
+        <h1 className="scroll-m-20 text-2xl text-slate-700 font-semibold tracking-tight lg:text-4xl">Case studies</h1>
+
+        <div className="flex items-center gap-2">
+          <button className="p-1 rounded-full border border-slate-300 text-slate-300 hover:bg-slate-800 hover:text-slate-100 hover:border-transparent">
+            <ChevronLeftIcon className="w-5 h-5" aria-hidden='true' />
+          </button>
+
+          <button className="p-1 rounded-full border border-slate-300 text-slate-300 hover:bg-slate-800 hover:text-slate-100 hover:border-transparent">
+            <ChevronRightIcon className="w-5 h-5" aria-hidden='true' />
+          </button>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
