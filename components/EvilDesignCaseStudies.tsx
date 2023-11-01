c54ab64be8c8a59550d93dd8fdf410268af356ff
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-01 16:19:23 +0300

    created the case studies array

--- components/EvilDesignCaseStudies.tsx
+++ components/EvilDesignCaseStudies.tsx
@@ -1,5 +1,22 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
 
+type CaseStudy = {
+  id: number
+  title: string
+  abbreviation: string
+  topic: string
+}
+
+const caseStudies: CaseStudy[] = [
+  { id: 1, title: 'Layers',  abbreviation: 'LA', topic: 'Business and Enterpreneurship' },
+  { id: 2, title: 'Capsule',  abbreviation: 'CA', topic: 'Education and Technology' },
+  { id: 3, title: 'FeatherDevs',  abbreviation: 'FE', topic: 'Marketing and Social Media' },
+  { id: 4, title: 'Command+R',  abbreviation: 'CR', topic: 'Human Resources' },
+  { id: 5, title: 'Tailwind',  abbreviation: 'TA', topic: 'Web Development' },
+  { id: 6, title: 'AlignUI',  abbreviation: 'AL', topic: 'Design Systems' },
+]
+
+
 export default function EvilDesignCaseStudies() {
   return (
     <section className="border-t border-t-slate-300 pt-2">
@@ -17,8 +34,12 @@ export default function EvilDesignCaseStudies() {
           <button className="p-1 rounded-full border border-slate-300 text-slate-300 hover:bg-slate-800 hover:text-slate-100 hover:border-transparent">
             <ChevronRightIcon className="w-5 h-5" aria-hidden='true' />
           </button>
+
         </div>
+
       </div>
+
+
 
     </section>
   )
